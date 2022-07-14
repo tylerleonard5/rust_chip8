870f97b307be5b3343f09c4d1c0d03feb78cd0cb
tylerleonard5 <tylerleonard@ufl.edu>
2022-07-14 19:52:44 -0400

    added data to chip8 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::Read;
+
+use chip8::Chip8;
+
+mod chip8;
+mod ram;
+
 fn main() {
-    println!("Hello, world!");
+    let mut gamefile = match File::open("gameData/INVADERS") {
+        Ok(file) => file,
+        Err(_) => panic!("Couldn't open file"),
+    };
+
+    let mut game_data = Vec::new();
+
+    gamefile.read_to_end(&mut game_data).expect("Couldn't read");
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&game_data);
 }
