f892ff4e2476172a7344e974090cfb60e0759f7d
tylerleonard5 <tylerleonard@ufl.edu>
2022-07-14 23:50:11 -0400

    starting over... you can do this

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use std::fs::File;
 use std::io::Read;
 
-use chip8::Chip8;
-
-mod chip8;
-mod ram;
-
 fn main() {
     let mut gamefile = match File::open("gameData/INVADERS") {
         Ok(file) => file,
@@ -15,12 +10,5 @@ fn main() {
     let mut game_data = Vec::new();
 
     gamefile.read_to_end(&mut game_data).expect("Couldn't read");
-
-    for i in &game_data {
-        println!("{:#X}", i);
-    }
-
-    let mut chip8 = Chip8::new();
-    chip8.load_rom(&game_data);
 
 }
--- target/debug/deps/rust_chip8-fc03ee749935ccae.d
+++ target/debug/deps/rust_chip8-fc03ee749935ccae.d
@@ -1,7 +1,5 @@
-d:\CodeProjects\Rust Chip8\chip8_emulator\target\debug\deps\rust_chip8-fc03ee749935ccae.rmeta: src\main.rs src\chip8.rs src\ram.rs
+d:\CodeProjects\Rust Chip8\chip8_emulator\target\debug\deps\rust_chip8-fc03ee749935ccae.rmeta: src\main.rs
 
-d:\CodeProjects\Rust Chip8\chip8_emulator\target\debug\deps\rust_chip8-fc03ee749935ccae.d: src\main.rs src\chip8.rs src\ram.rs
+d:\CodeProjects\Rust Chip8\chip8_emulator\target\debug\deps\rust_chip8-fc03ee749935ccae.d: src\main.rs
 
 src\main.rs:
-src\chip8.rs:
-src\ram.rs:
