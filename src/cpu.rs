2a7ee16e187a4f26563b549d8144560b9af3649a
tylerleonard5 <tylerleonard@ufl.edu>
2022-07-17 00:28:13 -0400

    Implement display

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,7 +6,7 @@ use crate::{chip8, mem::{self, Mem}};
 
 pub struct Cpu {
     regs: [u8; 16],
-    index: u16,
+    I: u16,
     pc: u16,
     stack: Vec<u16>,
     sp: u8,
@@ -17,7 +17,7 @@ impl Cpu {
     pub fn new() -> Cpu {
         Cpu { 
             regs: [0; 16],
-            index: 0,
+            I: 0,
             stack: Vec::new(),
             sp: 0,
             pc: chip8::PROGRAM_START,
@@ -37,17 +37,23 @@ impl Cpu {
 
         let instruction = (hi << 8) | lo;
 
-        let x = ((instruction & 0x0F00) << 4) >> 8;
-        let y = ((instruction & 0x00F0) << 8) >> 4;
+        let x = (instruction & 0x0F00) >> 8;
+        let y = (instruction & 0x00F0) >> 4;
 
-        let nnn = (instruction & 0x0FFF) << 4;
-        let nn = (instruction & 0x00FF) << 8;
-        let n = (instruction & 0x000F) << 12;
+        let nnn = instruction & 0x0FFF;
+        let nn = instruction & 0x00FF;
+        let n = instruction & 0x000F;
 
         let kind = (instruction & 0xF000) >> 12;
 
-        println!("{:#X}", instruction);
-        println!("{:#X}", kind);
+        // println!("{:#X}", instruction);
+        // println!("{:#X}", kind);
+        // println!("{:#X}", x);
+        // println!("{:#X}", y);
+        // println!("{:#X}", nnn);
+        // println!("{:#X}", nn);
+        // println!("{:#X}", n);
+
 
         match kind {
             0x0 => {
@@ -63,10 +69,21 @@ impl Cpu {
 
             0x1 => {
                 self.pc = nnn;
-                print!("TEST");
+            }
+            0x6 => {
+                self.regs[(x - 1) as usize] = nn as u8;
             }
 
+            0x7 => {
+                let ins = self.regs[(x - 1) as usize] + (nn as u8);
+                self.regs[(x - 1) as usize] = ins;
+            }
 
+            0xA => {
+                self.I = nnn;
+            }
+
+            //IMPLEMENT DISPLAY
 
             _ => print!("default")
 
